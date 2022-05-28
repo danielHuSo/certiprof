133a09fe82c18c552a485fb9f1e877c514d8321e
unknown <danielhs101@gmail.com>
2022-05-28 12:59:04 -0500

    Prueba de test fallido

--- src/test/java/com/tutorial/certiprof/CertiprofApplicationTests.java
+++ src/test/java/com/tutorial/certiprof/CertiprofApplicationTests.java
@@ -41,7 +41,7 @@ public class CertiprofApplicationTests {
         
         String result = greetingController.greeting(request, model);
         
-        assertEquals("greeting", result);
+        assertEquals("greeting11111", result);
 	}
 
 }
